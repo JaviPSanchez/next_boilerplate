4cc4826fcbbf3c58aee79ee6883abe6550650731
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2024-03-04 20:54:48 +0100

    Change package manager

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      script: 'yarn dev',
+      script: 'npm run dev',
     },
   ],
 
@@ -15,7 +15,7 @@ module.exports = {
       path: '/root/ATALAYA',
       'pre-deploy-local': '',
       'post-deploy':
-        'source ~/.nvm/nvm.sh && yarn install && yarn build && pm2 reload ecosystem.config.js --env production',
+        'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       ssh_options: 'ForwardAgent=yes',
     },
